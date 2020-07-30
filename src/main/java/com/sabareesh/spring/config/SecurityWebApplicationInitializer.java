428b0c0d2652ae09ab082bb1d195fb8232e10eff
sabareeshgovind <56748663+sabareeshgovind@users.noreply.github.com>
2020-07-30 14:35:59 -0400

    Role and user based security added to the REST API

---
+++ src/main/java/com/sabareesh/spring/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package com.sabareesh.spring.config;
+
+
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
